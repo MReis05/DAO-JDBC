743ce2faba62f11a831e9cd817de316d80057575
Matheus Reis <matheusreiscrs@gmail.com>
2024-12-17 08:59:32 -0300

    Creating class FindByDeparment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -10,9 +13,17 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("======= TESTE 1 FindByID =======");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
+		
+		System.out.println("======= TESTE 2 FindByDepartment");
+		Department dep = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(dep);
+		
+		for(Seller obj: list) {
+			System.out.println(obj);
+		}
 
 	}
 
