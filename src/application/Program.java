118155cb1ba3de553fdadaf3bd862fd89ac7e656
Matheus Reis <matheusreiscrs@gmail.com>
2024-12-19 08:47:07 -0300

    Update Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,12 +33,19 @@ public class Program {
 		for(Seller obj: list) {
 			System.out.println(obj);
 		}
-		
+		/*
 		System.out.println();
 		System.out.println("======= TESTE 3 Insert ============");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 3000.00, dep );
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted new Id: " + newSeller.getId());
+		*/
+		System.out.println();
+		System.out.println("======= TESTE 4 Update ============");
+		seller = sellerDao.findById(1);
+		seller.setNome("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update complete");
 	}
 
 }
